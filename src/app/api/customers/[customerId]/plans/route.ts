b9741a804f30362edae4b23e434dcf778eef8c4d
cleverBe <cleverbernal123@gmail.com>
2024-07-01 20:19:20 -0400

    endpoint para recuper los planes de cliente especifico

---
+++ src/app/api/customers/[customerId]/plans/route.ts
@@ -0,0 +1,64 @@
+import { getSessionServerSide } from "@/lib/getSession"
+import prismadb from "@/lib/prismadb"
+import { NextResponse } from "next/server"
+
+export const GET = async (
+  req: Request,
+  { params }: { params: { customerId: string } },
+) => {
+  try {
+    const session = await getSessionServerSide()
+
+    if (!session) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+    }
+
+    const customer = await prismadb.cliente.findUnique({
+      where: {
+        id: params.customerId,
+      },
+    })
+
+    if (!customer) {
+      return NextResponse.json(
+        { message: "Customer not found" },
+        { status: 404 },
+      )
+    }
+
+    const customerPlans = await prismadb.plan.findMany({
+      where: {
+        clienteId: customer.id,
+      },
+      include: {
+        asistencias: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    })
+
+    if (!customerPlans) {
+      return NextResponse.json(
+        { message: "Customer plans not found" },
+        { status: 404 },
+      )
+    }
+
+    return NextResponse.json({
+      id: customer.id,
+      nombre_completo: customer.nombre_completo,
+      genero: customer.genero,
+      celular: customer.celular,
+      cedula: customer.cedula,
+      fecha_nacimiento: customer.fecha_nacimiento,
+      planes: customerPlans,
+    })
+  } catch (error) {
+    console.log("[CUSTOMER-PLANS-GET]", error)
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 },
+    )
+  }
+}
